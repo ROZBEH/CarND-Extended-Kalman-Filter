f27cd19d18971d3d516c1217ee77c54efb4f8417
rozbeh <rozbeh@github.io>
2017-12-27 02:10:19 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Extended Kalman Filter Project
+
+[![Udacity - Self-Driving Car NanoDegree](https://s3.amazonaws.com/udacity-sdc/github/shield-carnd.svg)](http://www.udacity.com/drive)
+
+Overview
+---
+In this project we are going to use Extended Kalman Filter for tracking objects around self driving car. Laser and Radar data will be used as input to extended Kalman Filter. 
+Pipeline
+---
+
+*The overall pipeline along with the results will be described here!*
+
+<br>
+
+I. First sensor data will be read. Depending on the sensor type, we apply conversion to convert polar coordinate data into cartesian. For your note, Radar data is in polar coordinates and Laser data is in cartesian coordinates.
+
+II. Firt loocation (Px,Py) and velocity (Vx, Vy) of object will be predicted.
+
+III. Once the location is predicted, sensor input data will be used to update the location of the object. After each update step, update values will be pushed into an estimate array in order to calculate the root mean square error(rmse). As can be seen in the following youtube video the errors for (Px, Py, Vx, Vy) are around [] at the end of the simulation.
+
+IV. I also tried cases were just one of the sensors was active and measurements of the other senor was not considered during update and prediction step. We observe higher rmse when one of the sensors is inactive, this is because we have less information about the object, as a result there will be more uncertainty in calculations and the performance will be degraded. Comparing Laser and Radar based on rmse, it appears laser performs better because it has lower rmse error.
+
+<br>The following shows driving next to the edge of the curve and bringing the car back to the center of the road</br>
+<p align="center"><img src="examples/curv_1.jpg" width = "350" alt="Combined Image" />
+<img src="examples/curv_1_2.jpeg" width = "350" alt="Combined Image" /> </p>
+
+</br>
+<br></br>
+
+
---
+++ src/FusionEKF.cpp
@@ -0,0 +1,186 @@
+#include "FusionEKF.h"
+#include "tools.h"
+#include "Eigen/Dense"
+#include <iostream>
+#include <math.h> 
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::vector;
+
+/*
+ * Constructor.
+ */
+FusionEKF::FusionEKF() {
+  is_initialized_ = false;
+
+  previous_timestamp_ = 0;
+
+  // initializing matrices
+  
+  R_laser_ = MatrixXd(2, 2);
+  R_radar_ = MatrixXd(3, 3);
+  H_laser_ = MatrixXd(2, 4);
+  Hj_ = MatrixXd(3,4);
+
+
+  //measurement covariance matrix - laser
+  R_laser_ << 0.0225, 0,
+              0, 0.0225;
+
+  //measurement covariance matrix - radar
+  R_radar_ << 0.09, 0, 0,
+              0, 0.0006, 0,
+              0, 0, 0.09;
+  
+
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+        0, 1, 0, 1,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+  /**
+  TODO:
+    * Finish initializing the FusionEKF.
+    * Set the process and measurement noises
+  */
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
+
+
+
+
+  noise_ax = 9.0;
+  noise_ay = 9.0;
+}
+
+/**
+* Destructor.
+*/
+FusionEKF::~FusionEKF() {}
+
+void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
+
+
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+    /**
+    TODO:
+      * Initialize the state ekf_.x_ with the first measurement.
+      * Create the covariance matrix.
+      * Remember: you'll need to convert radar from polar to cartesian coordinates.
+    */
+    // first measurement
+
+    cout << "EKF: " << endl;
+    ekf_.x_ = VectorXd(4);
+    ekf_.x_ << 1, 1, 1, 1;
+
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+      Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      // Rouzbeh: Changing the radar coordinates into cartesian coordinates
+      float rho = measurement_pack.raw_measurements_[0];
+      float phi = measurement_pack.raw_measurements_[1];
+      float rho_dot = measurement_pack.raw_measurements_[2];
+      double x = rho * cos(phi);
+      if ( x < 0.0001 ) {
+        x = 0.0001;
+      }
+      double y = rho * sin(phi);
+      if ( y < 0.0001 ) {
+        y = 0.0001;
+      }
+      double vx = rho_dot * cos(phi);
+      double vy = rho_dot * sin(phi);
+      ekf_.x_ << x, y, vx , vy;
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+      Initialize state.
+      */
+      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+    }
+    // Deal with the special case initialisation problems
+    
+    previous_timestamp_ = measurement_pack.timestamp_;
+    // done initializing, no need to predict or update
+    ekf_.P_ = MatrixXd(4, 4);
+
+    is_initialized_ = true;
+    ekf_.P_ << 1, 0, 0, 0,
+              0, 1, 0, 0,
+              0, 0, 1000, 0,
+              0, 0, 0, 1000;
+    return;
+  }
+
+  /*****************************************************************************
+   *  Prediction
+   ****************************************************************************/
+
+  /**
+   TODO:
+     * Update the state transition matrix F according to the new elapsed time.
+      - Time is measured in seconds.
+     * Update the process noise covariance matrix.
+     * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
+   */
+
+  //compute the time elapsed between the current and previous measurements
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //dt - expressed in seconds
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+  float dt_44 = dt_4 / 4; 
+  float dt_32 = dt_3 / 2; 
+
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+  ekf_.Q_ = MatrixXd(4, 4);
+  //set the process covariance matrix Q
+  ekf_.Q_ << dt_44 * noise_ax, 0, dt_32 * noise_ax, 0,
+           0, dt_44 * noise_ay, 0, dt_32 * noise_ay,
+           dt_32 * noise_ax, 0, dt_2 * noise_ax, 0,
+           0, dt_32 * noise_ay, 0, dt_2 * noise_ay;
+
+
+  ekf_.Predict();
+  /*****************************************************************************
+   *  Update
+   ****************************************************************************/
+
+  /**
+   TODO:
+     * Use the sensor type to perform the update step.
+     * Update the state and covariance matrices.
+   */
+
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    // Radar updates
+    // If I don't want to use RADAR measurements I just use this simple return
+    // return;
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_; 
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+  } else {
+    // If I don't want to use LASER measurements I just use this simple return
+    // return;
+    // Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
+  }
+
+  // print the output
+  cout << "x_ = " << ekf_.x_ << endl;
+  cout << "P_ = " << ekf_.P_ << endl;
+}
